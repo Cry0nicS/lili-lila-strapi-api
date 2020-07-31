5d5caa35934158afbdf251308ac9070e8c4dc044
Adrian Popescu <adrian@gosugamers.net>
2020-07-31 23:41:06 +0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0b64f776ecd10f3165ad441b021e1389'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c29298ea-2eb2-4abf-98cd-ba0ace36393c'
+};
